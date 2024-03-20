47bea9630237a8a8334194d0fcfca98c9192b5f7
tuanngoc1601 <tuanngoc12340@gmail.com>
2024-03-20 15:44:45 +0700

    block scrolling behavior when the modal pops up

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect } from "react";
 
 const Modal = ({ children }: { children: React.ReactNode }) => {
     const router = useRouter();
@@ -17,13 +17,32 @@ const Modal = ({ children }: { children: React.ReactNode }) => {
         [router]
     );
 
+    useEffect(() => {
+        const body = document.querySelector("body");
+
+        if (body) {
+            body.style.overflow = "hidden";
+        }
+
+        return () => {
+            if (body) {
+                body.style.overflow = "auto";
+            }
+        };
+    }, []);
+
     return (
         <div className="w-full min-h-screen bg-bgModal backdrop-blur-sm fixed top-0 left-0 z-30 overflow-auto modal-content">
             <div
                 className="w-full min-h-screen top-0 left-0 absolute py-4 pb-25 px-17.5 z-30 flex flex-row justify-center items-center overflow-auto rounded-lg cursor-zoom-out"
                 onClick={(e) => backPreviouPage(e)}
             >
-                <div className="cursor-default" onClick={(e) => stopPropagation(e)}>{children}</div>
+                <div
+                    className="cursor-default"
+                    onClick={(e) => stopPropagation(e)}
+                >
+                    {children}
+                </div>
             </div>
         </div>
     );
